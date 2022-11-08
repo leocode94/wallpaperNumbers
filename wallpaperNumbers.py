2bb14b3580486acca24893c8a62f6595f99b727f
Leonardo Santana <50475381+leocode94@users.noreply.github.com>
2022-11-08 00:24:01 -0300

    Update wallpaperNumbers.py

--- wallpaperNumbers.py
+++ wallpaperNumbers.py
@@ -2,13 +2,14 @@
 import os
 import random
 # Assign the target directory
-directory = '/home/leonardo/Pictures/Wallpapers/'
+directory = '/home/leo/Pictures/Wallpapers/'
 # Iterate over the files in that directory
 for filename in os.scandir(directory):
     # Creates a random number
-    randomNumber = random.randrange(1000000, 9999999)
+    randomNumber = random.randrange(1000000000, 9999999999)
     # Transform the filename in a string
     file = str(filename)
+    # Check the type of the extensions
     if file.endswith(".jpg'>"):
         os.rename(filename, directory + str(randomNumber) + ".jpg")
     elif file.endswith(".png'>"):
@@ -19,3 +20,4 @@ for filename in os.scandir(directory):
         os.rename(filename, directory + str(randomNumber) + ".tif")
     elif file.endswith(".jfif'>"):
         os.rename(filename, directory + str(randomNumber) + ".jfif")
+print("All files have been renamed!")
